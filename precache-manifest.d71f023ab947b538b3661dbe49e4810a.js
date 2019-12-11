5812f6db8b95fac1dfa51b9e7fe5419c08336be9
projeto-ura <uragerente@gmail.com>
2019-12-11 00:12:13 -0300

    Updates

---
+++ precache-manifest.d71f023ab947b538b3661dbe49e4810a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0338eed3615a60a2dd6ade406ca8fcdd",
+    "url": "/ura-front/index.html"
+  },
+  {
+    "revision": "f45f7763cc2fdcc3d211",
+    "url": "/ura-front/static/css/main.99362dad.chunk.css"
+  },
+  {
+    "revision": "e62156f19a0601ecec9b",
+    "url": "/ura-front/static/js/2.f1dc9d49.chunk.js"
+  },
+  {
+    "revision": "f45f7763cc2fdcc3d211",
+    "url": "/ura-front/static/js/main.6dcd0242.chunk.js"
+  },
+  {
+    "revision": "34de7b3ba291cd04c33a",
+    "url": "/ura-front/static/js/runtime-main.0abdd5ef.js"
+  },
+  {
+    "revision": "1126c0dc57be6f7d196673136a5c978c",
+    "url": "/ura-front/static/media/01.1126c0dc.JPG"
+  },
+  {
+    "revision": "1aebf31ed530791af6b647116eb531c1",
+    "url": "/ura-front/static/media/02.1aebf31e.JPG"
+  },
+  {
+    "revision": "73102c1ba0551ed9a90df1a00e8b68fd",
+    "url": "/ura-front/static/media/03.73102c1b.JPG"
+  },
+  {
+    "revision": "ed4e1098635eed885656931e9bd40939",
+    "url": "/ura-front/static/media/04.ed4e1098.JPG"
+  },
+  {
+    "revision": "25500a5c46571ae7639404fb8349be22",
+    "url": "/ura-front/static/media/logo-facebookv2.25500a5c.png"
+  },
+  {
+    "revision": "5a2d5cfee6baaf7576f542f90cf31896",
+    "url": "/ura-front/static/media/logo-githubv2.5a2d5cfe.png"
+  },
+  {
+    "revision": "f29a34b7f2a44d418f108943498d7b29",
+    "url": "/ura-front/static/media/logo-instagramv2.f29a34b7.png"
+  },
+  {
+    "revision": "332ca7200820e853d93ebfdcbc86b507",
+    "url": "/ura-front/static/media/logo-ura.332ca720.png"
+  },
+  {
+    "revision": "a7eb41923d120c9973742501fadde997",
+    "url": "/ura-front/static/media/scholae_background2.a7eb4192.png"
+  },
+  {
+    "revision": "d83f15b80c968a5a0f408d1e12681bf9",
+    "url": "/ura-front/static/media/school.d83f15b8.svg"
+  },
+  {
+    "revision": "687ac6fdb5c48248f547011650ed4f38",
+    "url": "/ura-front/static/media/sensor.687ac6fd.jpg"
+  },
+  {
+    "revision": "855adeac81c7ea5413aa0f894c76a301",
+    "url": "/ura-front/static/media/study.855adeac.svg"
+  },
+  {
+    "revision": "fd0dee780c3c245bc722f6b1499e3dcd",
+    "url": "/ura-front/static/media/teacher.fd0dee78.svg"
+  },
+  {
+    "revision": "e44c8f30f0ba031fbcc48e09bb9baf24",
+    "url": "/ura-front/static/media/training.e44c8f30.svg"
+  },
+  {
+    "revision": "bb5e2ede589acaa28572de816f461f5d",
+    "url": "/ura-front/static/media/wallpaper.bb5e2ede.jpg"
+  }
+]);
